c811e683d29c281ce67d284516da9452aa6e715b
Maddyzeth <40778985+Maddyzeth@users.noreply.github.com>
2021-12-15 20:03:15 -0500

    Update logstash.tf

--- IAC/logstash.tf
+++ IAC/logstash.tf
@@ -1,5 +1,5 @@
 module "logstash" {
-  source = "git::https://github.com/AlienX456/SoundMonitor-IAC-Infrastructure-Common.git//use-cases/ecs_logstash_elastic_services?ref=3.0.12-rc"
+  source = "git::https://github.com/Maddyzeth/SoundMonitor-IAC-Infrastructure-Common.git//use-cases/ecs_logstash_elastic_services?ref=4.0.0"
 
   /*USER VARS*/
   cpu= "256"
